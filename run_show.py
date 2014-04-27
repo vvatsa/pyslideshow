3ca1b67830588032ef9511e0d8889f6ddd6d3b01
Vishal Vatsa <vva@renre.com>
2014-04-27 13:30:27 +0100

    simple slideshow for raspi
    Simple PyGame based photo slide show that will work on a RaspberyPi

---
+++ run_show.py
@@ -0,0 +1,65 @@
+import pygame
+import os
+import argparse
+import fnmatch
+
+
+def get_image_files(image_dir):
+
+    files = []
+    file_types = ["*.png", "*.jpeg", "*.jpg"]
+    for dirpath, dirnames, filenames in os.walk(image_dir):
+        for file_pattern in file_types:
+            files.extend([os.path.join(dirpath, fname) for fname in fnmatch.filter(filenames, file_pattern)])
+
+        for dirname in dirnames:
+            files.extend(get_image_files(os.path.join(dirpath, dirname)))
+
+    return files
+
+
+def display_image(img, screen):
+
+    scale_ratio = float(img.get_height()) / float(screen.get_height())
+    new_w = int(img.get_width() / scale_ratio)
+    new_h = int(img.get_height() / scale_ratio)
+    scaled = pygame.transform.scale(img, (new_w, new_h))
+
+    for ii in range(scaled.get_width() + screen.get_width(), 0, -5):
+        screen.blit(scaled, (-1 * ii , 0))
+        pygame.display.flip()
+
+
+
+
+def main():
+    parser = argparse.ArgumentParser("Raspberry Pi Photo slides show")
+    parser.add_argument("--image-dir", type=str, help="location of images.")
+    parser.add_argument("--on-pi", action="store_true", default=False)
+    parser.add_argument("--background-colour", default="250,250,250", help="background colour")
+    parser.add_argument("--resolution", default="1024x768", help="screen resolution.")
+    parser.add_argument("--delay", default=5, type=int)
+
+    opts = parser.parse_args()
+
+    if opts.on_pi:
+        os.putenv('SDL_VIDEODRIVER', 'fbcon')
+
+    pygame.init()
+
+    screen = pygame.display.set_mode([int(x) for x in opts.resolution.split('x')])
+
+    background = pygame.Surface(screen.get_size()).convert()
+    background.fill([int(x) for x in opts.background_colour.split(',')])
+
+    screen.blit(background, (0,0))
+    while True:
+        for image_file in get_image_files(opts.image_dir):
+            img = pygame.image.load(image_file).convert()
+            display_image(img, screen)
+            pygame.time.delay(opts.delay)
+
+
+if __name__ == "__main__":
+    print main()
+
